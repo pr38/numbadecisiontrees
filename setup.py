7bc93d3d889196bb79575f2d7cf4030581773aea
Pouya Rezvanipour <pouyar3@yahoo.com>
2023-09-06 15:12:53 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+install_requires = [
+    "numba",
+    "numpy",
+    "scikit-learn",
+]
+
+setup(
+    name="numbadecisiontrees",
+    version='0.0.1', 
+    description="novel 'numba' based recreation of scikit-learn's decision tree algorithm",
+    url="https://github.com/pr38/numbadecisiontrees",
+    author="Pouya Rezvanipour",
+    author_email="pouyar3@yahoo.com",
+    license="BSD",
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        'Development Status :: 3 - Alpha',
+    ],
+    install_requires = install_requires,
+    packages=["numbadecisiontrees"],
+)
