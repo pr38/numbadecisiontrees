f1dff209807c9663d419685db9be7cb97f1ce2ea
Pouya Rezvanipour <pouyar3@yahoo.com>
2023-09-07 16:17:22 -0700

    formating and variable name

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ install_requires = [
 
 setup(
     name="numbadecisiontrees",
-    version='0.0.1', 
+    version="0.0.1",
     description="novel 'numba' based recreation of scikit-learn's decision tree algorithm",
     url="https://github.com/pr38/numbadecisiontrees",
     author="Pouya Rezvanipour",
     author_email="pouyar3@yahoo.com",
     license="BSD",
-        classifiers=[
+    classifiers=[
         "Programming Language :: Python :: 3",
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 3 - Alpha",
     ],
-    install_requires = install_requires,
+    install_requires=install_requires,
     packages=["numbadecisiontrees"],
 )
